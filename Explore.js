ef20da6106ff3e8a501ac33d1b67818d486c3c0e
pavangit5 <pavansaipasupuleti@outlook.com>
2025-01-18 01:18:55 +0530

    this is change

---
+++ Explore.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('heading2')[0].innerHTML = 'Explore';
+
+a.addEventListener('click', function() {
+  document.getElementsByClassName('heading2')[0].innerHTML = 'Explore';
+});
