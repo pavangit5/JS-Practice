564903ea8cbb27592fe8c56dd9a5caf1a74c518e
pavangit5 <pavansaipasupuleti@outlook.com>
2025-01-18 01:22:09 +0530

    This is an message

--- Explore.js
+++ Explore.js
@@ -3,3 +3,5 @@ document.getElementsByClassName('heading2')[0].innerHTML = 'Explore';
 a.addEventListener('click', function() {
   document.getElementsByClassName('heading2')[0].innerHTML = 'Explore';
 });
+
+here it is an anoterh change
